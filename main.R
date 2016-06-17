771deb73682d43f8d15f7cf46dc9cea767b7017a
bsnpavan <bsnpavan@gmail.com>
2016-06-17 12:54:44 +0530

    Updates from ShareLaTeX

--- main.R
+++ main.R
@@ -1,6 +1,12 @@
+library("pastecs")
+
 traindata<- read.csv("loan_train.csv")
 head(traindata)
 traindata
-head(traindata, 1)
-
-filter(traindata, Loan_ID:Loan_Status) %>% 
+head(traindata, 5)
+str(traindata)
+train_cont<- subset(traindata,select= c(ApplicantIncome, LoanAmount, Loan_Amount_Term, Credit_History))
+summary(train_cont)
+options(scipen=100)
+options(digits=2)
+stat.desc(train_cont)
